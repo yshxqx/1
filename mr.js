994449f282364ea0bf50e083760bccd8edafbe23
yshxqx <69250113+yshxqx@users.noreply.github.com>
2026-06-22 15:07:05 +0800

    更新 mr.js

--- mr.js
+++ mr.js
@@ -6,11 +6,11 @@
 
  [Script]
  # 会员
- http-response ^https:\/\/([a-z0-9-]+\.cloudfront\.net|7yhgyt\.com|jnhb8y79\.com|ql2u9\.com|3e8qs\.com|cpmg23\.com|g7926j\.com)\/api\/app\/mine\/info$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=会员
+ http-response ^https:\/\/([a-z0-9-]+\.cloudfront\.net|7yhgyt\.com|jnhb8y79\.com|ql2u9\.com|3e8qs\.com|cpmg23\.com|g7926j\.com)\/api\/app\/mine\/info$ script-path=https://raw.githubusercontent.com/yshxqx/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=会员
  # 视频
- http-response ^https:\/\/([a-z0-9-]+\.cloudfront\.net|7yhgyt\.com|jnhb8y79\.com|ql2u9\.com|3e8qs\.com|cpmg23\.com|g7926j\.com)\/api\/app\/vid\/info\?data=.*$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=视频
+ http-response ^https:\/\/([a-z0-9-]+\.cloudfront\.net|7yhgyt\.com|jnhb8y79\.com|ql2u9\.com|3e8qs\.com|cpmg23\.com|g7926j\.com)\/api\/app\/vid\/info\?data=.*$ script-path=https://raw.githubusercontent.com/yshxqx/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=视频
  # 广告
-http-response ^https:\/\/([a-z0-9-]+\.cloudfront\.net|7yhgyt\.com|jnhb8y79\.com|ql2u9\.com|3e8qs\.com|cpmg23\.com|g7926j\.com)\/api\/app\/(ping\/domain\/h5|recreation\/list_v2)$ script-path=https://raw.githubusercontent.com/chmg2025/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=广告
+http-response ^https:\/\/([a-z0-9-]+\.cloudfront\.net|7yhgyt\.com|jnhb8y79\.com|ql2u9\.com|3e8qs\.com|cpmg23\.com|g7926j\.com)\/api\/app\/(ping\/domain\/h5|recreation\/list_v2)$ script-path=https://raw.githubusercontent.com/yshxqx/js/refs/heads/main/mr.js, requires-body=true, timeout=60, tag=广告
 
 
  [MITM]
